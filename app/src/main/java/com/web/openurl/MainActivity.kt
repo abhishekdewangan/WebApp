3e67e5765ab88223d3dfb31ec7ecfe319dd61c75
praveen dewangan <pdewangan@mersat-tech.com>
2018-09-02 14:53:21 +0530

    added web view to show urls

---
+++ app/src/main/java/com/web/openurl/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.web.openurl
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupWebView()
+        webview.loadUrl("https://google.com")
+    }
+
+    private fun setupWebView() {
+        webview.setWebViewClient(WebViewClient())
+        webview.settings.javaScriptEnabled = true
+        webview.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+    }
+
+    override fun onBackPressed() {
+        if (webview.canGoBack()) {
+            webview.goBack()
+        } else {
+            finish()
+        }
+    }
+}
